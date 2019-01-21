b2abb971de7a80be6cb274dc7e119988336ca08d
Drew Maughan <drew.maughan@hackney.gov.uk>
2019-01-21 11:22:58 +0000

    Progression up to Lecture 28.

--- src/app/06-services/todos.component.ts
+++ src/app/06-services/todos.component.ts
@@ -2,24 +2,28 @@
 import { TodoService } from './todo.service'
 
 export class TodosComponent { 
-  todos = [];
-  message: string; 
+	todos: any[] = [];
+	message: string; 
 
-  constructor(private service: TodoService) {}
+	constructor(private service: TodoService) {}
 
-  ngOnInit() { 
-    this.service.getTodos().subscribe(t => this.todos = t);
-  }
+	ngOnInit() { 
+		this.service.getTodos().subscribe(
+			(t: any[]) => { this.todos = t; }
+		);
+	}
 
-  add() { 
-    var newTodo = { title: '... ' };
-    this.service.add(newTodo).subscribe(
-      t => this.todos.push(t),
-      err => this.message = err);
-  }
+	add() { 
+		var newTodo = { title: '... ' };
+		this.service.add(newTodo).subscribe(
+			(t: any) => { this.todos.push(t); },
+			(err: string) => { this.message = err; }
+		);
+	}
 
-  delete(id) {
-    if (confirm('Are you sure?'))
-      this.service.delete(id).subscribe();
-  }  
+	delete(id) {
+		if (confirm('Are you sure?')) {
+			this.service.delete(id).subscribe();
+		}
+	}  
 }
